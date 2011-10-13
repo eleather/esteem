1c99a92d546fca3ea842160ab05934bf6cda9f02
Emily L <peppermintdove@gmail.com>
2011-10-12 20:47:58 -0700

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Esteem::Application.config.secret_token = '5cfa57e09192c08b241ed69fcdf2f6aee9d60cbd522aa1b7039b369ad293458055d2a599e36daafcc0ef8cfe3819eb76c534234957e2cfac3a8982439bc507d4'
