6497e6de781328be4b67cbb749d10a53983073cb
Emily L <peppermintdove@gmail.com>
2011-10-26 19:15:52 -0700

    don't show descriptions in list views

--- spec/views/organizations/index.html.haml_spec.rb
+++ spec/views/organizations/index.html.haml_spec.rb
@@ -22,7 +22,5 @@ describe "organizations/index.html.haml" do
     assert_select "tr>td", :text => "Name".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Slug".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Description".to_s, :count => 2
   end
 end
--- spec/views/projects/index.html.haml_spec.rb
+++ spec/views/projects/index.html.haml_spec.rb
@@ -25,8 +25,6 @@ describe "projects/index.html.haml" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Slug".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Description".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => 1.to_s, :count => 2
   end
 end
