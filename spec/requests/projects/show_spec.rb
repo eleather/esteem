64a3bcfb86e16959763cfcb0922c6d860364ff53
Emily L <peppermintdove@gmail.com>
2011-10-27 21:58:12 -0700

    more detailed spec

--- spec/requests/projects/show_spec.rb
+++ spec/requests/projects/show_spec.rb
@@ -8,9 +8,6 @@ describe "projects/show.html.haml" do
     visit project_path(@project)
   end
   
-  
-  ### Correctly Display Project Attributes ###
-  
   it 'should render the correct record attributes' do
     page.should have_content(@project.name)
     page.should have_content(@project.slug)
@@ -18,26 +15,44 @@ describe "projects/show.html.haml" do
     page.should have_link(@project.organization.name, :href => organization_path(@project.organization))
   end
   
-  
-  ### Dashboard Aspects ###
-  
-  it 'should display recent question responses aggregated by radial'
-  it 'should display the 8 most recent suggestions for this project'
-  
-  describe 'when the user has recently answered no questions for this project' do
-    it 'should display all questions for the user to answer'
+  describe 'in #radials section'
+    it 'should display recent question responses aggregated by radial'
+    it 'should have a link to a page giving more information about the radials for this project'
+    it 'should have a link to a view showing the trends of question responses aggregated by radial over time'
   end
   
-  describe 'when the user has recently answered only some questions for this project' do
-    it 'should display only unanswered questions for the user to answer'
+  describe 'in #suggestions section'
+    describe 'when this project has no suggestions'
+      it 'should display the new suggestion box at the top of the section'
+      it 'should have no other content'
+    end
+    
+    describe 'when this project has less than 8 suggestions'
+      it 'should display the new suggestion box at the top of the section'
+      it 'should display all suggestions for this project, in most-voted order'
+    end
+
+    describe 'when this project has more than 8 suggestions'
+      it 'should display the new suggestion box at the top of the section'
+      it 'should display the 8 most recent suggestions for this project, in most-voted order'
+    end
   end
   
-  describe 'when the user has recently answered all questions for this project' do
-    it 'should display text telling the user that they\'ve answered all the questions for now'
+  describe 'in #questions section'
+    describe 'when this project has no questions' do
+      it 'should display text telling the user that there are no questions for this project'
+    end
+  
+    describe 'when the user has recently answered no questions for this project' do
+      it 'should display all questions for the user to answer'
+    end
+  
+    describe 'when the user has recently answered only some questions for this project' do
+      it 'should display only unanswered questions for the user to answer'
+    end
+  
+    describe 'when the user has recently answered all questions for this project' do
+      it 'should display text telling the user that they\'ve answered all the questions for now'
+    end
   end
-  
-  
-  ### Links to Other Views ###
-  
-  it 'should have a link to a view showing the trends of question responses aggregated by radial over time'
 end
